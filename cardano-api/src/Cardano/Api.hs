275f3664f21b0a6cbdae7c6671ad57510ee0320b
John Ky <john.ky@iohk.io>
2021-05-11 13:05:36 +1000

    In query tip command, make epoch number available on Byron

--- cardano-api/src/Cardano/Api.hs
+++ cardano-api/src/Cardano/Api.hs
@@ -498,6 +498,10 @@ module Cardano.Api (
     toNetworkMagic,
     --TODO: Remove after updating cardano-node-chairman with new IPC
     SomeNodeClientProtocol(..),
+
+    SlotsInEpoch(..),
+    SlotsToEpochEnd(..),
+    slotToEpoch,
   ) where
 
 import           Cardano.Api.Address
@@ -518,6 +522,7 @@ import           Cardano.Api.Modes
 import           Cardano.Api.NetworkId
 import           Cardano.Api.OperationalCertificate
 import           Cardano.Api.ProtocolParameters
+import           Cardano.Api.Query (SlotsInEpoch(..), SlotsToEpochEnd(..), slotToEpoch)
 import           Cardano.Api.Script
 import           Cardano.Api.SerialiseBech32
 import           Cardano.Api.SerialiseCBOR
